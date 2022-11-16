cebf8a6695f0c68e5962176b33e195266475fb4b
Roni Castro <roni.c.castro@gmail.com>
2022-11-15 22:58:21 -0300

    refactor: allow RemoteAuthetication to pass body and response data type

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,13 +1,14 @@
 import { HttpPostClient, HttpResponseStatus } from '@/data/protocols/http'
 import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models'
+import { Authentication } from '@/domain/usecases'
 
 export type AuthenticationParams = {
   email: string
   password: string
 }
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
     private readonly httpClient: HttpPostClient<
@@ -16,11 +17,12 @@ export class RemoteAuthentication {
     >
   ) {}
 
-  async auth(params: AuthenticationParams): Promise<void> {
+  async auth(params: AuthenticationParams): Promise<AccountModel> {
     const response = await this.httpClient.post({ url: this.url, body: params })
     switch (response.statusCode) {
       case HttpResponseStatus.Success:
-        break
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        return response.data!
       case HttpResponseStatus.Unauthorized:
         throw new InvalidCredentialsError()
       default:
