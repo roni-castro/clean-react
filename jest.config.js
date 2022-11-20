2421cc4e60d81901f060d083f7bf5949b2d3e881
Roni Castro <roni.c.castro@gmail.com>
2022-11-19 22:48:21 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: 'ts-jest',
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
   }
 }
