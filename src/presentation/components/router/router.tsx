3e89f6d259daae5e263ce9d9fc618e1cf7a67997
Roni Castro <roni.castro@devbase.us>
2022-11-22 20:04:24 -0300

    test: ensure email change calls validation

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -2,11 +2,18 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Login } from '@/presentation/pages'
 
+class Validation {
+  input?: object
+  errorMessage?: string
+
+  validate(input: object) {}
+}
+
 export default function Router() {
   return (
     <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/login' element={<Login />} />
+      <Route path='/' element={<Login validation={new Validation()} />} />
+      <Route path='/login' element={<Login validation={new Validation()} />} />
     </Routes>
   )
 }
--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { faker } from '@faker-js/faker'
+import { Validation } from '@/presentation/protocols'
 import { Login } from './login'
+
+class ValidationSpy implements Validation {
+  input?: object
+  errorMessage?: string
+
+  validate(input: object) {
+    this.input = input
+
+    return this.errorMessage
+  }
+}
+
+const makeSut = () => {
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation={validationSpy} />)
+  return { sut, validationSpy }
+}
 
 describe('Login', () => {
   it('should start with initial state', () => {
-    render(<Login />)
-
+    makeSut()
     expect(screen.queryByTestId('login-spinner')).toBeFalsy()
     expect(screen.queryByTestId('login-error')).toBeFalsy()
     expect(screen.getByRole('button', { name: 'Entrar' })).toBeDisabled()
@@ -17,5 +36,15 @@ describe('Login', () => {
     const passwordStatus = screen.getByTestId('password-status')
     expect(passwordStatus).toHaveTextContent('🔴')
     expect(passwordStatus.title).toBe('Campo obrigatório')
+  })
+
+  it('should call Validation with correct email', async () => {
+    const { validationSpy } = makeSut()
+    const email = faker.internet.email()
+    const emailInput = screen.getByTestId('email')
+
+    await userEvent.type(emailInput, email)
+
+    expect(validationSpy.input).toEqual({ email })
   })
 })
