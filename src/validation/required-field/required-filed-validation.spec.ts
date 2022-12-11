f8b967067c5fcf138f467d4c13db2f081cb28feb
Roni Castro <roni.c.castro@gmail.com>
2022-12-11 02:04:01 -0300

    refactor: add sut for the RequiredFieldValidation test

--- src/validation/required-field/required-filed-validation.spec.ts
+++ src/validation/required-field/required-filed-validation.spec.ts
@@ -2,15 +2,17 @@ import { faker } from '@faker-js/faker'
 import { RequiredFieldError } from '@/validation/errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = () => new RequiredFieldValidation(faker.database.column())
+
 describe('RequiredFieldValidation', () => {
   it('should return error if field is empty', () => {
-    const sut = new RequiredFieldValidation(faker.database.column())
+    const sut = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
   })
 
   it('should return falsy if field is not invalid', () => {
-    const sut = new RequiredFieldValidation(faker.database.column())
+    const sut = makeSut()
     const error = sut.validate(faker.random.word())
     expect(error).toBeFalsy()
   })
