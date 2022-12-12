0ebc04508b24de57dd6003dcc9869eb60a1af565
Roni Castro <roni.c.castro@gmail.com>
2022-12-12 00:11:28 -0300

    refactor: make tests simpler and improve descriptions

--- src/validation/validation-composite/validation-composite.spec.ts
+++ src/validation/validation-composite/validation-composite.spec.ts
@@ -1,61 +1,70 @@
 import { faker } from '@faker-js/faker'
-import { FieldValidation } from '../protocols'
 import { FieldValidationSpy } from '../tests'
 import { ValidationComposite } from './validation-composite'
 
-const makeSut = (validators: FieldValidation[]) => {
-  return new ValidationComposite(validators)
+const makeSut = (customValidators: FieldValidationSpy[] = []) => {
+  const fieldName = faker.database.column()
+  const fieldValidationsSpy = [
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName),
+    ...customValidators
+  ]
+
+  return {
+    sut: new ValidationComposite(fieldValidationsSpy),
+    fieldName,
+    fieldValidationsSpy
+  }
 }
 
 describe('ValidationComposite', () => {
-  it('should return first error message if any validation fails', () => {
-    const fieldName = faker.database.column()
-    const fieldValidation1Spy = new FieldValidationSpy(fieldName)
-    const fieldValidation2Spy = new FieldValidationSpy(fieldName)
-    const sut = makeSut([fieldValidation1Spy, fieldValidation2Spy])
+  describe('when there are multiple errors for the same field', () => {
+    it('should return first error message', () => {
+      const { sut, fieldName, fieldValidationsSpy } = makeSut()
 
-    fieldValidation1Spy.error = new Error('field_validation_1_error_message')
-    fieldValidation2Spy.error = new Error('field_validation_2_error_message')
+      fieldValidationsSpy[0].error = new Error(
+        'field_validation_1_error_message'
+      )
+      fieldValidationsSpy[1].error = new Error(
+        'field_validation_2_error_message'
+      )
 
-    const error = sut.validate({
-      fieldName,
-      fieldValue: faker.random.word()
+      const error = sut.validate({
+        fieldName,
+        fieldValue: faker.random.word()
+      })
+      expect(error).toEqual('field_validation_1_error_message')
     })
-    expect(error).toEqual('field_validation_1_error_message')
   })
 
-  it('should return first error message if any validation fails', () => {
-    const fieldName = faker.database.column()
-    const fieldValidation1Spy = new FieldValidationSpy(fieldName)
-    const fieldValidation2Spy = new FieldValidationSpy(fieldName)
-    const sut = makeSut([fieldValidation1Spy, fieldValidation2Spy])
+  describe('when there is one error and it is the second validator', () => {
+    it('should return this second validator error message', () => {
+      const { sut, fieldName, fieldValidationsSpy } = makeSut()
 
-    fieldValidation2Spy.error = new Error('field_validation_2_error_message')
+      fieldValidationsSpy[1].error = new Error(
+        'field_validation_2_error_message'
+      )
 
-    const error = sut.validate({
-      fieldName,
-      fieldValue: faker.random.word()
+      const error = sut.validate({
+        fieldName,
+        fieldValue: faker.random.word()
+      })
+      expect(error).toEqual('field_validation_2_error_message')
     })
-    expect(error).toEqual('field_validation_2_error_message')
-  })
 
-  it('should return falsy if all validations for that field succeeds', () => {
-    const fieldName = faker.database.column()
-    const fieldValidation1Spy = new FieldValidationSpy(fieldName)
-    const fieldValidation2Spy = new FieldValidationSpy(fieldName)
-    const fieldValidation3Spy = new FieldValidationSpy(faker.database.column())
-    const sut = makeSut([
-      fieldValidation1Spy,
-      fieldValidation2Spy,
-      fieldValidation3Spy
-    ])
+    it('should return falsy if all validations for that field succeeds', () => {
+      const otherFieldValidationSpy = new FieldValidationSpy(
+        faker.database.column()
+      )
+      const { sut, fieldName } = makeSut([otherFieldValidationSpy])
 
-    fieldValidation3Spy.error = new Error('any_error_message')
+      otherFieldValidationSpy.error = new Error('other_field_error_message')
 
-    const error = sut.validate({
-      fieldName,
-      fieldValue: faker.random.word()
+      const error = sut.validate({
+        fieldName,
+        fieldValue: faker.random.word()
+      })
+      expect(error).toBeFalsy()
     })
-    expect(error).toBeFalsy()
   })
 })
