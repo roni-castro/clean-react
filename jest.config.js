8390a29b935056454a67cef3184530c0585fa0f8
Roni Castro <roni.c.castro@gmail.com>
2023-01-22 15:41:12 -0300

    chore: remove main layer from test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}', 
+    '!<rootDir>/src/main/**/*', 
+    '!**/*.d.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
